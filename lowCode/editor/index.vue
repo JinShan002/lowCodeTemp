873910ee5746f95bd19dbc367f788e3012546f58
archer <zhengkaida@xinyuehulian.com>
2023-01-13 17:30:10 +0800

    增加lerna以及服务端脚本

---
+++ lowCode/editor/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="page-wrap">
+    <el-button @click="openAdd">新建</el-button>
+    <el-dialog
+      v-model="dialogVisible"
+      title="Tips"
+      width="30%"
+    >
+      <span>This is a message</span>
+
+    </el-dialog>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+function openAdd() {
+  dialogVisible.value = true;
+}
+
+const dialogVisible = ref(false);
+
+</script>
+
+<style scoped lang="scss"></style>
